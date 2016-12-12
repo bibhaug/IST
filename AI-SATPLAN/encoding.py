cf1c9044ca266f2f323a7ae969b86aa9225d02d0
bibhaug <bibhaug.github@gmail.com>
2016-12-12 20:41:05 +0000

    fixed merge problems, conversionDicts

--- AI-SATPLAN/encoding.py
+++ AI-SATPLAN/encoding.py
@@ -5,23 +5,12 @@ from hebrand_base import actionNames
 from read_pddl_domain_file import info_from_file
 
 def encodingHandler():
-<<<<<<< HEAD
-		initial_sat_set = createSatDict()
-		#print('Initial sat_set is: ', initial_sat_set)
-		all_actions, init_atoms, goal_state = info_from_file()
-		initial_state_CNF = createInitialStateCnfSentence(initial_sat_set, init_atoms)
-		print('Initial cnf is: ', initial_state_CNF)
-		#print('Length of initial CNF is: ', len(initial_state_CNF))
-
-#         her kommer en while løkke som kaller dpll og slikt
-=======
-        initial_sat_set = createSatDict()
+        initial_sat_set_atoms, initial_sat_set_numbers = createConversionDicts()
         #print('Initial sat_set is: ', initial_sat_set)
         all_actions, init_atoms, goal_state = info_from_file()
-        initial_state_CNF = createInitialStateCnfSentence(initial_sat_set, init_atoms)
+        initial_state_CNF = createInitialStateCnfSentence(initial_sat_set_atoms, init_atoms)
         print('Initial cnf is: ', initial_state_CNF)
         #print('Length of initial CNF is: ', len(initial_state_CNF))
->>>>>>> refs/remotes/origin/bibh
 
 # def linear_encoder(horizon): #lager final sat_sentence for hver horizon, og returner til encoding_handler som kaller opp dpll() og slikt
 #         #rekkefølgen ting må gjøres i for å lage sat_sentence, bruker mange av hjelpefunksjonene under
@@ -33,89 +22,11 @@ def encodingHandler():
 
 #         return sat_sentence
 
-<<<<<<< HEAD
-def createSatDict():
-=======
+
 def createConversionDicts():
->>>>>>> refs/remotes/origin/kari
         #I had to use lists to change the names (since you can't iterate through a set)
         #In case we want the values in the dictionary to be integers instead of strings: dict_with_ints = dict((k,int(v)) for k,v in dict_with_strs.iteritems())
-        #Atoms_to_numbers_dict 
-
-<<<<<<< HEAD
-		action_names = actionNames()
-		for i in range(0, len(action_names)):
-				action_names[i] = action_names[i] + '0'
-		hebrand_base_set = hebrandBase()
-		hebrand_base_list = list(hebrand_base_set)
-		for i in range (0, len(hebrand_base_list)):
-				hebrand_base_list[i] = hebrand_base_list[i] + '0'
-		hebrand_base_dict = dict.fromkeys(hebrand_base_list, 0)
-		for i in range (0, len(action_names)):
-				hebrand_base_dict[action_names[i]] = 0
-		value = 1
-		hebrand_base_dict_copy = {}
-		for key, val in hebrand_base_dict.items():
-				if key.startswith('-'):
-						key1 = key
-						key2 = key[1:]
-						if key1 not in hebrand_base_dict_copy or key2 not in hebrand_base_dict_copy:
-								hebrand_base_dict_copy[key2] = str(value)
-								hebrand_base_dict_copy[key1] = '-' + str(value)
-								value += 1 
-				else:
-						key1 = key
-						key2 = '-' + key
-						if key1 not in hebrand_base_dict_copy or key2 not in hebrand_base_dict_copy:
-								hebrand_base_dict_copy[key1] = str(value)
-								hebrand_base_dict_copy[key2] = '-' + str(value)
-								value += 1 
-		print('SAT_DICTIONARY: ', hebrand_base_dict_copy)
-		return hebrand_base_dict_copy
-
-def extendSatSet(horizon, old_sat_set):
-		#utvid med ett timestep
-		#return sat_set_dict
-		h = horizon
-		sat_dict = old_sat_set
-		new_keys = []
-		max_values_key = max(sat_dict, key = sat_dict.get)
-		max_value = sat_dict[max_values_key]
-		for key, val in sat_dict.items():
-				new_key = key[:-1] + str(h)
-				new_keys.append(new_key)
-		for i in range(0,len(new_keys)):
-				sat_dict[new_keys[i]] = max_value + (i + 1)
-		return new_sat_dict
-
-
-
-def createInitialStateCnfSentence(sat_set_dict, init_states):
-		init_cnf = []
-		single_object_clause = ['init']
-		copy_sat_set_dict = copy.deepcopy(sat_set_dict)
-		copy_init_states = copy.deepcopy(init_states)
-		#print('Copy of sat_set_dict: ', copy_sat_set_dict)
-		print('Init states: ', init_states)
-		for atom in range(len(copy_init_states)):
-				copy_init_states[atom] = copy_init_states[atom] + '0'
-				for key, val in copy_sat_set_dict.items():
-						print('Copy_init_states[atom]: ', copy_init_states[atom])				
-						if key == copy_init_states[atom]:
-								print('ATOM MATCH FOUND')
-								single_object_clause = key
-								print('Single_object_clause match is: ', single_object_clause)
-								init_cnf.append(single_object_clause)
-								print('Init_CNF after match append: ', init_cnf)
-						else:
-								#print('Atom match not found')
-								single_object_clause = '-' + key
-								#print('Single object clause is: ', single_object_clause)
-								init_cnf.append(single_object_clause)
-		print('Init_CNF: ', init_cnf)
-		return init_cnf
-		# def createGoalStateCnfSentence(goal_states, horizon):
-=======
+        #Atoms_to_numbers_dict
         action_names = actionNames()
         for i in range(0, len(action_names)):
                 action_names[i] = action_names[i] + '0'
@@ -190,9 +101,7 @@ def extendConversionDicts(horizon, old_atoms_to_num_dict, old_num_to_atoms_dict)
                                 num_dict[str(val2)] = key2
         print('This is Atom Dict: ', atom_dict)
         print('This is Num Dict: ', num_dict)
-        return atom_dict
-
-
+        return atom_dict, num_dict
 
 def createInitialStateCnfSentence(sat_set_dict, init_states):
         init_cnf = []
@@ -220,7 +129,6 @@ def createInitialStateCnfSentence(sat_set_dict, init_states):
         return init_cnf
 
 # def createGoalStateCnfSentence(goal_states, horizon):
->>>>>>> refs/remotes/origin/kari
 #         Vil ha det på liste-form
 #         return goal_states_list
 
@@ -258,7 +166,7 @@ def actionSatToCnf(sat_sentence):
         print('CNF-expression: ',cnf_expression)
 #         return cnf_expression #cnf-expression er en liste i liste  [[1,2,8],[-3,-4,-6]] der komma tilsvarer "or"
 
-# def frameAxiomSatToCnf(sat_sentence, current_sat_dict):
+# def frameAxiomSatToCnf(sat_sentence, current_atom_dict, current_num_dict):
 #         #sat_sentence = [['23'], ['30']] (der 23 er et atom fra hebrand base og 30 er en action)
 #         atom = sat_sentence[0]
 #         atom_negated = '-' + atom[0]
